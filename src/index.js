b06df44d011a56804c4324c83eb2ca7ddcf5c4c9
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-05-05 17:40:13 -0400

    all requirement done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 // write your code here
+
+//Get all images from the server
+fetch('http://localhost:3000/ramens')
+.then(res => res.json())
+.then(ramens => ramens.forEach(ramen => uploadRamenImages(ramen)))
+
+document.querySelector('#new-ramen').addEventListener('submit', event => {
+    
+    event.preventDefault()
+    let newRamen = {
+        name: event.target.name.value,
+        restaurant: event.target.restaurant.value,
+        image: event.target.image.value,
+        rating: event.target.rating.value,
+        comment: event.target.comment.value
+    }
+    //console.log(newRamen)
+    uploadRamenImages(newRamen)
+})
+
+//Update Reman's rating and comment
+document.querySelector('#edit-ramen').addEventListener('submit', event => {
+    event.preventDefault()
+    document.querySelector('#rating-display').textContent = event.target.rating.value
+    document.querySelector('#comment-display').innerHTML = `${event.target.comment.value}`
+
+})
+
+
+
+
+function uploadRamenImages(ramen){
+    let image = document.createElement('img')
+    image.src = ramen.image
+    document.querySelector('#ramen-menu').appendChild(image)
+
+    image.addEventListener('click', event => {
+        document.querySelector('.detail-image').src = ramen.image
+        document.querySelector('.name').textContent = ramen.name
+        document.querySelector('.restaurant').textContent = ramen.restaurant
+        document.querySelector('#rating-display').textContent = ramen.rating
+        document.querySelector('#comment-display').textContent = ramen.comment
+    })
+
+}
